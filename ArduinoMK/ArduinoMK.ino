301c9b1f51efa235d5b164ad446108dd73f6d8a3
Henry J Schmale <hschmale16@gmail.com>
2015-02-10 18:55:48 -0500

    Finished Documentation block of Arduino Sketch

--- ArduinoMK/ArduinoMK.ino
+++ ArduinoMK/ArduinoMK.ino
@@ -2,6 +2,24 @@
  * Keyboard - Custom Chorded Type
  * Mouse - JoyStick
  *
+ * \file ArduinoMK.ino
  * \author Henry J Schmale
  * \date 10 February 2015
+ *
+ * Circuit:
+ *    * 2-axis Joystick Pot with Select
+ *        * x-axis is on A0
+ *        * y-axis is on A1
+ *        * Select is on interupt 0, Pin 3 on Leonardo, Pin 2 on Uno
+ *    * 7 Momementary Closed SPST Push Button Switches
  */
+
+
+
+void setup(){
+    
+}
+
+void loop(){
+    
+}
