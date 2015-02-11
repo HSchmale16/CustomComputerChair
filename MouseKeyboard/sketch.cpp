54bbf7a656733ef1f0c061721372b250bff308e7
Henry J Schmale <hschmale16@gmail.com>
2015-02-10 22:57:34 -0500

    New stuff
    * got rid of html build log, stupid codeblocks templetes
    * Made an upload script, b/c the upload command is really friggin long
    * Updated gitignore for html that doesn't belong

--- MouseKeyboard/sketch.cpp
+++ MouseKeyboard/sketch.cpp
@@ -1,22 +1,16 @@
 #include <Arduino.h>
 
-/*
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
-*/
+/** A Custom Mouse & Keyboard for custom gaming chair
+ * \author Henry J Schmale
+ * \date 10 February 2015
+ */
 
 void setup()
 {
-	// initialize the digital pin as an output.
-	// Pin 13 has an LED connected on most Arduino boards:
-	pinMode(13, OUTPUT);     
+
 }
 
 void loop()
 {
-	digitalWrite(13, HIGH);   // set the LED on
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // set the LED off
-	delay(1000);              // wait for a second
+
 }
---
+++ uploadUno.sh
@@ -0,0 +1,2 @@
+avrdude -v -v -v -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:MouseKeyboard/bin/Release/MouseKeyboard.elf.hex:i
+
