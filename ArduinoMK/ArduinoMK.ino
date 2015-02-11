718ba024581fe0e0132e50ee3caac43469a8e92e
Henry J Schmale <hschmale16@gmail.com>
2015-02-10 19:27:42 -0500

    Selective Compilation

--- ArduinoMK/ArduinoMK.ino
+++ ArduinoMK/ArduinoMK.ino
@@ -12,9 +12,19 @@
  *        * y-axis is on A1
  *        * Select is on interupt 0, Pin 3 on Leonardo, Pin 2 on Uno
  *    * 7 Momementary Closed SPST Push Button Switches
+ *    * Control Input SPST is on Interupt 1, Pin 2 Leonardo, Pin 3 Uno
  */
 
+// This is the board that we are currently targeting, UNO or LEONARDO
+#define UNO
+#ifdef UNO // if uno is targeted
 
+#endif
+#ifdef LEONARDO // if the leonardo is targeted
+
+#endif
+
+volatile bool keyboardActive = false;
 
 void setup(){
     
@@ -23,3 +33,5 @@ void setup(){
 void loop(){
     
 }
+
+
