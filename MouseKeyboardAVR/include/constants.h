559a344c77e65a2105b5bfdaa1d0b2f2e1777d7c
Henry J Schmale <hschmale16@gmail.com>
2015-02-11 20:24:55 -0500

    Started Laying out keyboard matrix

--- MouseKeyboardAVR/include/constants.h
+++ MouseKeyboardAVR/include/constants.h
@@ -6,5 +6,11 @@
  * \date 11 February 2015
  */
 
+// Keyboard Matrix
+const char KEYBOARD_MATRIX[3][16] = {
+    {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'},
+    {'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', ';', '?', '"', },
+    {}
+};
 
 #endif // CONSTANTS_H_INCLUDED
--- MouseKeyboardAVR/main.cpp
+++ MouseKeyboardAVR/main.cpp
@@ -2,6 +2,8 @@
 #include <util/delay.h>
 #include <Arduino.h>
 
+#include "include/constants.h"
+
 int main(void)
 {
     init();
