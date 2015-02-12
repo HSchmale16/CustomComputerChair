f637ddc51d7c8d2ea1cdea3f133c82bb6d5249ab
Henry J Schmale <hschmale16@gmail.com>
2015-02-11 20:34:39 -0500

    New Code Files
    * Codified the some files
    * Finished typing out the keyboard matrix
    * Added files to contain primary implementation

---
+++ MouseKeyboardAVR/include/implement.h
@@ -0,0 +1,8 @@
+#ifndef IMPLEMENT_H_INCLUDED
+#define IMPLEMENT_H_INCLUDED
+
+namespace km{
+    int initKM();
+}
+
+#endif // IMPLEMENT_H_INCLUDED
---
+++ MouseKeyboardAVR/src/implement.cpp
@@ -0,0 +1,8 @@
+/** \file src/implement.cpp
+ * \author Henry J Schmale
+ * \date 11 February 2015
+ */
+
+#include "../include/implement.h" //!< This contains prototypes
+#include "../include/constants.h" //!< Various Constants
+#include <Arduino.h>              //!< Nice Library, Easy to u
