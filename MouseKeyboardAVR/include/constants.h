c8b4206b931217810fc62bb06167e55c17497f1d
Henry J Schmale <hschmale16@gmail.com>
2015-02-11 20:02:25 -0500

    A Constants Header is added

---
+++ MouseKeyboardAVR/include/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+/** Contains Various Constants for controlling the Arduino Mouse Keyboard Combo
+ * \author Henry J Schmale
+ * \date 11 February 2015
+ */
+
+
+#endif // CONSTANTS_H_INCLUDED
--- MouseKeyboardAVR/main.cpp
+++ MouseKeyboardAVR/main.cpp
@@ -13,6 +13,7 @@ int main(void)
         digitalWrite(13, LOW);
         delay(500);
         digitalWrite(13, HIGH);
+        delay(500);
     }
     return 0;
 }
