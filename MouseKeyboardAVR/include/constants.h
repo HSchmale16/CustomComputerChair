058c0dad64f7c801e9ff35f6ad00d511803a88e2
Henry J Schmale <hschmale16@gmail.com>
2015-02-12 22:05:15 -0500

    Schematic is almost done
    YAY!
    
    Just have to make a bill of materials

--- MouseKeyboardAVR/include/constants.h
+++ MouseKeyboardAVR/include/constants.h
@@ -8,12 +8,12 @@
  */
 
 // ===== Pin Constants ======
-const int BUTTON_0 = 3;
-const int BUTTON_1 = 4;
-const int BUTTON_2 = 5;
-const int BUTTON_3 = 6;
-const int BUTTON_4 = 8;
-const int BUTTON_5 = 9;
-const int SPACE_ENTER = 10;
+const int BUTTON_0 = 3;     //!< SW_1
+const int BUTTON_1 = 4;     //!< SW_2
+const int BUTTON_2 = 5;     //!< SW_3
+const int BUTTON_3 = 6;     //!< SW_4
+const int BUTTON_4 = 8;     //!< SW_5
+const int BUTTON_5 = 9;     //!< SW_6
+const int SPACE_ENTER = 10; //!< SW_7
 
 #endif // CONSTANTS_H_INCLUDED
