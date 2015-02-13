96df384af5a7ec523ed40dd3f8ee0c207dc81fa0
Henry J Schmale <hschmale16@gmail.com>
2015-02-13 16:13:58 -0500

    Working Joystick Prototype
    Ammendment:
    This is really important! The joystick switch pin must be configured as INPUT_PULLUP

--- Test_Progs/JoystickTest/JoystickTest.ino
+++ Test_Progs/JoystickTest/JoystickTest.ino
@@ -4,9 +4,19 @@
  */
 
 void setup(){
-  
+  Serial.begin(115200);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A5, INPUT_PULLUP);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, HIGH);
 }
 
 void loop(){
-  
+  Serial.print("Vx = ");
+  Serial.print(analogRead(A3));
+  Serial.print("\t\tVy = ");
+  Serial.print(analogRead(A4));
+  Serial.print("\t\tSwitchState = ");
+  Serial.println(digitalRead(A5));
 }
