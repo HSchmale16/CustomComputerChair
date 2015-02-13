0b1466e8b850cd5bc1df418f7984107106abd208
Henry J Schmale <hschmale16@gmail.com>
2015-02-12 20:57:01 -0500

    Constants
    * Constants in header
    * USB Schematic

--- MouseKeyboardAVR/include/constants.h
+++ MouseKeyboardAVR/include/constants.h
@@ -8,6 +8,12 @@
  */
 
 // ===== Pin Constants ======
-
+const int BUTTON_0 = 3;
+const int BUTTON_1 = 4;
+const int BUTTON_2 = 5;
+const int BUTTON_3 = 6;
+const int BUTTON_4 = 8;
+const int BUTTON_5 = 9;
+const int SPACE_ENTER = 10;
 
 #endif // CONSTANTS_H_INCLUDED
